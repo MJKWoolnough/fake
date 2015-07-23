2ece7547a96727203666f5269fff96c2ef4a8e93
Michael Woolnough <michael.woolnough@gmail.com>
2015-07-23 19:36:22 +0100

    Initial dilesystem code

---
+++ os/filesystem.go
@@ -0,0 +1,66 @@
+package os
+
+import "time"
+
+type metadata struct {
+	name    string
+	size    int64
+	mode    FileMode
+	modTime time.Time
+}
+
+func (m metadata) Name() string {
+	return m.Name()
+}
+
+func (m metadata) Size() int64 {
+	return m.size
+}
+
+func (m metadata) Mode() FileMode {
+	return m.mode
+}
+
+func (m metadata) IsDir() bool {
+	return m.mode.IsDir()
+}
+
+type file struct {
+	metadata
+	data []byte
+}
+
+func newFile(d *directory, name string, mode FileMode, modTime time.Time, contents []byte) {
+
+}
+
+func (f file) Sys() interface{} {
+	return f.data
+}
+
+type directory struct {
+	metadata
+	parent, self *directory
+	contents     []FileInfo
+}
+
+func newDirectory(d *directory, name string, mode FileMode, modTime time.Time, contents []FileInfo) {
+
+}
+
+func (d directory) Sys() interface{} {
+	return d.contents
+}
+
+type symbolic struct {
+	metadata
+	link string
+}
+
+func newSymbolic(d *directory, name string, mode FileMode, modTime time.Time, contents string) {
+
+}
+
+func (s symbolic) Sys() interface{} {
+	return s.link
+}
