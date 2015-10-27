4664937204aba1319ac72f2bcb22bee0ed84771c
Michael Woolnough <michael.woolnough@gmail.com>
2015-10-27 20:19:00 +0000

    Start of filesystem implementation

--- os/filesystem.go
+++ os/filesystem.go
@@ -1,232 +1,104 @@
 package os
 
 import (
+	"io"
 	"os"
-	"sort"
 	"sync"
 	"time"
-
-	"github.com/MJKWoolnough/memio"
 )
 
-var (
-	root = &directory{
-		node{
-			os.ModeDir | 0777,
-			time.Now(),
-			"",
-			nil,
-		},
-		make(map[string]os.FileInfo),
-	}
-	cwdmu sync.Mutex
-	cwd   *directory
-)
+var fs = struct {
+	sync.Mutex
+	root directory
+	cwd  path
+}{
+	root: dir{},
+	cwd:  path{},
+}
 
-func init() {
-	root.parent = root
-	cwd = root
-	Mkdir("/tmp", 0777)
-	Chdir("/tmp")
+type data interface {
+	Size() int64
+	Mode() os.FileMode
+	ModTime() time.Time
+	Data()
 }
 
 type node struct {
-	os.FileMode
-	modTime time.Time
-	name    string
-	parent  *directory
+	name string
+	data
 }
 
-func (n node) Name() string {
+func (n *node) Name() string {
 	return n.name
 }
 
-func (n node) Mode() os.FileMode {
-	return n.FileMode
+func (n *node) IsDir() bool {
+	return n.Mode().IsDir()
 }
 
-func (n node) ModTime() time.Time {
-	return n.modTime
+func (n *node) IsSymlink() bool {
+	return n.Mode()&ModeSymlink > 0
 }
 
-func (n *node) chmod(fileMode os.FileMode) error {
-	n.FileMode = fileMode | (n.FileMode & os.ModeDir)
-	return nil
+func (n *node) Sys() interface{} {
+	return n.data
 }
 
-func (n *node) setModTime(m time.Time) {
-	n.modTime = m
+type file interface {
+	io.Reader
+	io.Writer
+	io.Seeker
+	io.Closer
+	io.ReaderAt
+	io.WriterAt
+	io.ReaderFrom
+	io.WriterTo
+	Sync() error
+	Truncate(int64) error
+	WriteString(string) (int, error)
 }
 
-func (n *node) move(name string, d *directory) error {
-	if n.parent == nil {
-		return ErrInvalid
-	}
-	if !canRead(n.parent.FileMode) || !canWrite(d.FileMode) {
-		return ErrPermission
-	}
-	f, ok := n.parent.Contents[n.name]
-	if !ok {
-		return ErrInvalid
-	}
-	delete(n.parent.Contents, n.name)
-	n.parent = d
-	d.Contents[name] = f
-	return nil
+type directory interface {
+	io.Seeker
+	io.Closer
+	Readdir(int) ([]FileInfo, error)
+	Readdirnames(int) ([]string, error)
 }
 
-type directory struct {
-	node
-	Contents map[string]os.FileInfo
+type link interface {
+	Follow() node
 }
 
-func namecheck(name string) error {
-	for _, c := range name {
-		switch c {
-		case '\x00', '/':
-			return ErrInvalid
-		}
-	}
-	return nil
+type path struct {
+	name   string
+	deep   uint
+	parent *path
 }
 
-func (d *directory) create(name string, perm os.FileMode) (os.FileInfo, error) {
-	if !canWrite(d.FileMode) {
-		return nil, ErrPermission
-	}
-	if f, ok := d.Contents[name]; ok {
-		return f, nil
-	}
-	if err := namecheck(name); err != nil {
-		return nil, err
-	}
-	f := &file{
-		node{
-			perm &^ os.ModeDir,
-			time.Now(),
-			name,
-			d,
-		},
-		make([]byte, 0),
-	}
-	d.Contents[name] = f
-	return f, nil
+type modeTime struct {
+	FileMode
+	modTime time.Time
 }
 
-func (d *directory) mkdir(name string, fileMode os.FileMode) (*directory, error) {
-	if !canWrite(d.FileMode) {
-		return nil, ErrPermission
-	}
-	if _, ok := d.Contents[name]; ok {
-		return nil, ErrExist
-	}
-	if err := namecheck(name); err != nil {
-		return nil, err
-	}
-	e := &directory{
-		node{
-			fileMode | os.ModeDir,
-			time.Now(),
-			name,
-			d,
-		},
-		make(map[string]os.FileInfo),
-	}
-	d.Contents[name] = e
-	return e, nil
+func (m modeTime) Mode() os.FileMode {
+	return m.FileMode
 }
 
-func (d *directory) get(name string) (os.FileInfo, error) {
-	if !canExecute(d.FileMode) {
-		return nil, ErrPermission
-	}
-	switch name {
-	case ".":
-		return d, nil
-	case "..":
-		return d.parent, nil
-	}
-	fi, ok := d.Contents[name]
-	if !ok {
-		return nil, ErrNotExist
-	}
-	return fi, nil
+func (m modeTime) ModTime() time.Time {
+	return m.modTime
 }
 
-func (d *directory) remove(name string, all bool) error {
-	if !canWrite(d.FileMode) {
-		return ErrPermission
-	}
-	fi, ok := d.Contents[name]
-	if !ok {
-		return ErrNotExist
-	}
-	if fi.IsDir() {
-		dir := fi.(*directory)
-		if len(dir.Contents) > 0 {
-			if all {
-				for name := range dir.Contents {
-					err := dir.remove(name, true)
-					if err != nil {
-						return err
-					}
-				}
-			} else {
-				return ErrNotEmpty
-			}
-		}
-	}
-	delete(d.Contents, name)
-	return nil
+type dir struct {
+	modeTime
+	contents []FileInfo
 }
 
-func (d *directory) Size() int64 {
-	return 0
+type fileBytes struct {
+	modeTime
+	data []byte
 }
 
-func (d *directory) Sys() interface{} {
-	return d.Contents
-}
-
-func (d *directory) getContents(flag int) (contents, error) {
-	if flag&O_WRONLY != 0 {
-		return nil, ErrIsDir
-	}
-	list := make([]os.FileInfo, 0, len(d.Contents))
-	for _, fi := range d.Contents {
-		list = append(list, fi)
-	}
-	dir := &directoryC{list}
-	sort.Sort(dir)
-	return dir, nil
-}
-
-type file struct {
-	node
-	Contents []byte
-}
-
-func (f *file) Size() int64 {
-	return int64(len(f.Contents))
-}
-
-func (f *file) Sys() interface{} {
-	return &f.Contents
-}
-
-func (f *file) getContents(flag int) (contents, error) {
-	rw := readWrite{memio.OpenMem(&f.Contents)}
-	if flag&O_TRUNC != 0 {
-		f.Contents = f.Contents[:0]
-	}
-	if flag&O_APPEND != 0 {
-		rw.Seek(0, 2)
-	}
-	if flag&O_RDWR != 0 {
-		return rw, nil
-	}
-	if flag&O_WRONLY != 0 {
-		return noRead{rw}, nil
-	}
-	return noWrite{rw}, nil
+type fileString struct {
+	modeTime
+	data string
 }
