5428f25d14f923989e332824b060ceec20229b7d
Michael Woolnough <michael.woolnough@gmail.com>
2015-10-31 16:03:26 +0000

    Implemented more of OpenFile and added readOnly and writeOnly file wrappers

--- os/file.go
+++ os/file.go
@@ -3,25 +3,69 @@ package os
 import (
 	"io"
 	"os"
+	"path"
+	"time"
 )
 
 func Create(name string) (*File, error) {
-
+	return OpenFile(name, O_RDWR|O_CREATE|O_TRUNC, 0666)
 }
 
 func NewFile(fd uintptr, name string) *File {
-
+	return nil
 }
 
 func Open(name string) (*File, error) {
-
+	return OpenFile(name, O_RDONLY, 0)
 }
 
 func OpenFile(name string, flag int, perm os.FileMode) (*File, error) {
+	n, err := fs.getNode(name, true)
+	if err != nil {
+		if IsNotExist(err) && flag&O_CREATE != 0 {
+			dname, fname := path.Split(name)
+			d, err := fs.getDirectory(dname)
+			if err != nil {
+				return &PathError{
+					"open",
+					name,
+					err,
+				}
+			}
+			n = &fileBytes{
+				modeTime: modeTime{
+					FileMode(perm),
+					time.Now(),
+				},
+				data: make([]byte, 0),
+			}
+			if err = d.set(fname, n); err != nil {
+				return &PathError{
+					"open",
+					name,
+					err,
+				}
+			}
+		} else {
+			return &PathError{
+				"open",
+				name,
+				err,
+			}
+		}
+	} else if flag&O_EXCL == 0 {
+		return &PathError{
+			"open",
+			name,
+			ErrExist,
+		}
+	}
 
+	return nil, nil
 }
 
 type File struct {
+	name string
 }
 
 type fileWrapper struct {
@@ -95,4 +139,48 @@ func (dirWrapper) WriteString(string) (int, error) {
 
 func (dirWrapper) WriteTo(io.Writer) (int, error) {
 	return 0, ErrIsDir
+}
+
+type readOnly struct {
+	file
+}
+
+func (readOnly) ReadFrom(io.Reader) (int64, error) {
+	return 0, ErrPerm
+}
+
+func (readOnly) Sync() error {
+	return nil
+}
+
+func (readOnly) Truncate(int64) error {
+	return ErrPerm
+}
+
+func (readOnly) Write([]byte) (int, error) {
+	return 0, ErrPerm
+}
+
+func (readOnly) WriteAt([]byte, int64) (int, error) {
+	return 0, ErrPerm
+}
+
+func (readOnly) WriteString(string) (int, error) {
+	return 0, ErrPerm
+}
+
+type writeOnly struct {
+	file
+}
+
+func (writeOnly) Read([]byte) (int, error) {
+	return 0, ErrPerm
+}
+
+func (writeOnly) ReadAt([]byte, int64) (int, error) {
+	return 0, ErrPerm
+}
+
+func (writeOnly) WriteTo(io.Writer) (int, error) {
+	return 0, ErrPerm
 }
